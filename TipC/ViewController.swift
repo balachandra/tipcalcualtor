424929861e492aa1af396d1f6663adfb9a64ab05
Balachandra Maddina <Balachandra_maddina@intuit.com>
2017-03-03 21:53:45 -0800

    Initial Commit

---
+++ TipC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipC
+//
+//  Created by Maddina, Balachandra on 3/3/17.
+//  Copyright © 2017 Maddina, Balachandra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
